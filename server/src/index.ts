84c7ffc9b55620b0794beb79253cad183aada68b
Sam-Alwin <samalwin777@gmail.com>
2025-03-12 11:43:33 +0530

    Backend deploy
    new repo to use render

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "./routes";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(router);
+app.use(cors({
+  origin: "http://coursetracker2.s3-website.eu-north-1.amazonaws.com:5173", // Allow frontend requests
+  credentials: true,
+}));
+
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on port 5000");
+});
+
